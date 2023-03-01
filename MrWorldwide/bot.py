e18dfc4b79bfdf92823cb21b730a5eddb63b6308
Fleapous <girayegecan02@gmail.com>
2023-03-01 23:48:46 +0100

    bot can follow you around and can respond to events like muting

--- MrWorldwide/bot.py
+++ MrWorldwide/bot.py
@@ -12,7 +12,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_bot():
-    TOKEN = 'MTA3OTQwNzMyMjgwNTMyNTkyNA.GSrY8b.KxIl8P8ie49co9HBQ6y-79WWYTdpZ0lHPl-pKg'
+    TOKEN =
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
@@ -40,7 +40,50 @@ def run_bot():
         else:
             await send_message(msg, user_message, is_private=False)
 
+    @client.event
+    async def on_voice_state_update(member, before, after):
+        if before.channel is None and after.channel is not None:  # User joined voice channel
+            voice_channel = after.channel
+            await voice_channel.connect()
 
+        elif after.channel is None and before.channel is not None:  # User left voice channel
+            voice_client = client.voice_clients[0]
+            await voice_client.disconnect()
+
+    @client.event
+    async def on_voice_state_update(member, before, after):
+        if before.channel is None and after.channel is not None:  # User joined voice channel
+            voice_channel = after.channel
+            await voice_channel.connect()
+
+        elif after.channel is None and before.channel is not None:  # User left voice channel
+            voice_client = client.voice_clients[0]
+            await voice_client.disconnect()
+
+        elif before.self_mute != after.self_mute or before.self_deaf != after.self_deaf:  # User muted or deafened themselves
+            voice_client = client.voice_clients[0]
+            if voice_client.is_connected():
+                await voice_client.move_to(None)  # Disconnect and reconnect to update user's mute/deaf status
+
+    @client.event
+    async def on_voice_state_update(member, before, after):
+        if before.channel is None and after.channel is not None:  # User joined voice channel
+            voice_channel = after.channel
+            await voice_channel.connect()
+
+        elif after.channel is None and before.channel is not None:  # User left voice channel
+            voice_client = client.voice_clients[0]
+            await voice_client.disconnect()
+
+        elif before.self_mute != after.self_mute or before.self_deaf != after.self_deaf:  # User muted or deafened themselves
+            voice_client = client.voice_clients[0]
+            if voice_client.is_connected():
+                await voice_client.move_to(None)  # Disconnect and reconnect to update user's mute/deaf status
+
+        elif before.channel != after.channel:  # User switched voice channels
+            voice_client = client.voice_clients[0]
+            if voice_client.is_connected():
+                await voice_client.move_to(after.channel)  # Move to the new voice channel
     # Remember to run your bot with your personal TOKEN
     client.run(TOKEN)
 
